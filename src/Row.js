ace07e7a1b494bfc4b1017943fc7387e07f0f393
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-02-23 13:54:26 +0100

    Row component fetching data from TMDB
    - title
    - image

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
 
-function Row({ title }) {
+const baseImagesUrl = "https://image.tmdb.org/t/p/original";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // if [] -> run once when the Row oads and dont run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      // console.table(request.data.results);
+
+      return request;
+    }
+
+    fetchData();
+  }, [fetchUrl]);
+
+  // console.log(movies);
+
   return (
-    <div>
-      { title }
+    <div className="row">
+      <h2> {title} </h2>
+
+      <div className="rows__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${baseImagesUrl}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
