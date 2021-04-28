f729c57b201f0bb269407f564467287fd9bda8be
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-04-28 11:00:35 +0200

    Trying to add yutube plugin to preview videos

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ function App() {
         isLargeRow // same as isLargeRow={true}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      {/* <Row title="Top Rated" fetchUrl={requests.fetchTopRated} /> */}
+      {/* <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} /> */}
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const baseImagesUrl = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     // if [] -> run once when the Row oads and dont run again
@@ -22,6 +25,31 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
   // console.log(movies);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          // https://www.youtube.com/watch?v=XtMThy8QKqU
+          // XtMThy8QKqU -> video id
+          const urlParams = new URL(url).search; // '?v=XtMThy8QKqU'
+          const urlSearcher = new URLSearchParams(urlParams); // URLSearchParams { 'v' => 'XtMThy8QKqU' }
+          const youtubeMovieId = urlSearcher.get("v");
+          setTrailerUrl(youtubeMovieId);
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2> {title} </h2>
@@ -29,7 +57,8 @@ function Row({ title, fetchUrl, isLargeRow }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-            id={movie.id}
+            key={movie.id}
+            onClick={handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${baseImagesUrl}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -38,6 +67,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
